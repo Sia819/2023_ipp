e12cbc3ba8c8c2fd5516ef0b452f5166e30d06d6
lunasia819 <lunasia819@gmail.com>
2023-08-02 18:00:17 +0900

    스폰 소스 수정중

--- JSG/Assets/01 Scripts/Mob/MobSpawn.cs
+++ JSG/Assets/01 Scripts/Mob/MobSpawn.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MobSpawn : MonoBehaviour
 {
@@ -12,32 +12,45 @@ public class MobSpawn : MonoBehaviour
     [field: SerializeField] public GameObject MobZomBunny { get; set; }
     [field: SerializeField] public GameObject MobHellephant { get; set; }
 
-    Vector3 SpawnArea { get; set; }
-
     // Start is called before the first frame update
     void Start()
     {
         if (GameManager.Instance.IsPlaying)
         {
-            //StartCoroutine(Spawn());
+            StartCoroutine(Spawn());
         }
     }
 
-    //IEnumerator Spawn()
-    //{
-    //    while (GameManager.Instance.IsPlaying)
-    //    {
-    //        
-    //
-    //         //Vector3 MapCenter.position.x + 10
-    //
-    //
-    //        //Instantiate()
-    //    }
-    //}
+    IEnumerator Spawn()
+    {
+        while (GameManager.Instance.IsPlaying)
+        {
+            Vector3 SpawnPoint;
 
-    //Vector3 GenRandomPoint(Vector3 basePosition)
-    //{
-    //    float 
-    //}
+            // 官蹿 积己
+            SpawnPoint = GenRandomPoint(SpawnFloor);
+
+            // 零备港 积己
+            //SpawnPoint = GenRandomPoint(SpawnHole);
+            Instantiate(MobZomBear, SpawnPoint, Quaternion.identity);
+
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    Vector3 GenRandomPoint(Collider spawnArea)
+    {
+        Vector3 newPoint;
+
+        float rangeX = spawnArea.bounds.size.x / 4;
+        float rangeZ = spawnArea.bounds.size.z / 4;
+
+        // 官款靛冠胶 郴 罚待 困摹 蔼 积己
+        float randomX = Random.Range(-rangeX, rangeX);
+        float randomZ = Random.Range(-rangeZ, rangeZ);
+
+        newPoint = spawnArea.bounds.center + new Vector3(randomX, 0f, randomZ);
+
+        return newPoint;
+    }
 }
