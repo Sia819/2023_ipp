0790b5901fbcec2b8cab3a8b6bb633e53b89016b
lunasia819 <lunasia819@gmail.com>
2023-08-02 17:31:53 +0900

    몹스폰 생성중 콜라이더 생성완료

--- JSG/Assets/01 Scripts/Mob/MobSpawn.cs
+++ JSG/Assets/01 Scripts/Mob/MobSpawn.cs
@@ -1,23 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MobRespawn : MonoBehaviour
+public class MobSpawn : MonoBehaviour
 {
+    [field: SerializeField] public Collider SpawnFloor { get; set; }
+    [field: SerializeField] public Collider SpawnHole { get; set; }
     [field: SerializeField] public Transform Player { get; set; }
+    [field: SerializeField] public GameObject MobZomBear { get; set; }
+    [field: SerializeField] public GameObject MobZomBunny { get; set; }
+    [field: SerializeField] public GameObject MobHellephant { get; set; }
+
+    Vector3 SpawnArea { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
         if (GameManager.Instance.IsPlaying)
         {
-            
+            //StartCoroutine(Spawn());
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //IEnumerator Spawn()
+    //{
+    //    while (GameManager.Instance.IsPlaying)
+    //    {
+    //        
+    //
+    //         //Vector3 MapCenter.position.x + 10
+    //
+    //
+    //        //Instantiate()
+    //    }
+    //}
+
+    //Vector3 GenRandomPoint(Vector3 basePosition)
+    //{
+    //    float 
+    //}
 }
